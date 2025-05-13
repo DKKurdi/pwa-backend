0cf37444f7b26a54689bb1a1bd72e660b47fa0f1
DKKurdi <128648584+DKKurdi@users.noreply.github.com>
2025-05-14 00:50:27 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
